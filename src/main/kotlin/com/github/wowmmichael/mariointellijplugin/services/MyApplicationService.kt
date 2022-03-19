24e68fb2e7e9cce1abbf2998e451f961c12c1a68
GitHub Action <action@github.com>
2022-03-19 15:21:53 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/wowmmichael/mariointellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.wowmmichael.mariointellijplugin.services
+
+import com.github.wowmmichael.mariointellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
